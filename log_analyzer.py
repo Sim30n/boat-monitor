221aff0118883c8cc51dbff9b67ce151c26f9b83
Petteri <42471379+Sim30n@users.noreply.github.com>
2023-06-08 17:01:11 +0000

    Send info with telegram

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,5 +1,8 @@
 import datetime
 import sys
+import pandas as pd
+import telegram_send
+
 
 def read_log_file():
     file1 = open("app.log", "r")
@@ -24,8 +27,43 @@ for line in lines:
     to_dict = eval(dict_string)
     log_items.append(to_dict)
 
-for log_item in log_items:
-    try:
-        print(log_item[sys.argv[1]])
-    except KeyError:
-        print("Give valid log item.")
+# --------------- pandas stuff ------------------------
+df = pd.DataFrame(log_items)
+df["electric_load"] = pd.to_numeric(df["electric_load"], downcast="float")
+df["battery_voltage"] = pd.to_numeric(df["battery_voltage"], downcast="float")
+df["water_temperature"] = pd.to_numeric(df["water_temperature"], downcast="float")
+df["inside_temperature"] = pd.to_numeric(df["inside_temperature"], downcast="float")
+df["humidity_temperature"] = pd.to_numeric(df["humidity_temperature"], downcast="float")
+df["bilge_pump_run_time"] = pd.to_numeric(df["bilge_pump_run_time"], downcast="float")
+
+
+# Set the 'date' column as the index
+df.set_index("time_stamp", inplace=True)
+# Define your desired date range
+current_datetime = pd.to_datetime(pd.Timestamp.now())
+start_date = current_datetime - pd.Timedelta(days=1)
+rows_between_dates = df.loc[start_date:current_datetime]
+
+average_battery_voltage = rows_between_dates["battery_voltage"].mean()
+average_electric_load = rows_between_dates["electric_load"].mean()
+average_water_temperature = rows_between_dates["water_temperature"].mean()
+average_inside_temperature = rows_between_dates["inside_temperature"].mean()
+average_humidity_temperature = rows_between_dates["humidity_temperature"].mean()
+average_bilge_pump_run_time = rows_between_dates["bilge_pump_run_time"].mean()
+send_info = f"""Past 24h averages
+Battery voltage: {average_battery_voltage}
+Electric load: {average_electric_load}
+Water temperature: {average_water_temperature}
+Inside temperature: {average_inside_temperature}
+Inside humidity: {average_humidity_temperature}
+Average bilge pump run time: {average_bilge_pump_run_time}"""
+print(send_info)
+telegram_send.send(messages=[send_info])
+
+
+# ---- print all -------------
+#for log_item in log_items:
+#    try:
+#        print(log_item[sys.argv[1]])
+#    except KeyError:
+#        print("Give valid log item.")
