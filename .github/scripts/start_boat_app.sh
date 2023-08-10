97aa53da53f8946d1713d23c2efbb6d22a2b753d
Petteri <42471379+Sim30n@users.noreply.github.com>
2023-08-10 17:35:30 +0000

    Add pipeline stuff

---
+++ .github/scripts/start_boat_app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the session name
+SESSION_NAME="boat-app"
+
+# Kill the session if it already exists
+tmux has-session -t $SESSION_NAME 2>/dev/null
+if [ $? != 0 ]; then
+    tmux kill-session -t $SESSION_NAME
+fi
+
+# Start a new tmux session with the defined name
+tmux new-session -d -s $SESSION_NAME
+
+# Send a command to the tmux session to start the Python program
+tmux send-keys -t $SESSION_NAME "./home/pi/projects/boat/boat-monitor/venv/bin/python3 your_python_script.py" C-m
+
+# Detach the tmux session
+tmux detach-session -t $SESSION_NAME
