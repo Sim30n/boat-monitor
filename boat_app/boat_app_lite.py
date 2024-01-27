e716f02454374edeeee5baabf8e89d17279999a8
Petteri Särkkä <petteri.sarkka@gmail.com>
2024-01-27 21:52:26 +0200

    fix path

--- boat_app/boat_app_lite.py
+++ boat_app/boat_app_lite.py
@@ -82,7 +82,7 @@ class SeeeduinoBoard:
         self.ser.close()
 
 def find_usb_ports():
-    run_command = subprocess.Popen("arduino-cli board list", shell=True, stdout=subprocess.PIPE).stdout.read()
+    run_command = subprocess.Popen("/home/pi/arduino-cli/bin/arduino-cli board list", shell=True, stdout=subprocess.PIPE).stdout.read()
     for line in run_command.decode().splitlines():
         if "Arduino Micro" in line:
             arduino_micro = line[:12]
